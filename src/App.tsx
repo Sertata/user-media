96fd9d1361c3ad2aba213e52b0581a6f2bff65d8
StrelkovSS <sergeistrelkov98@gmail.com>
2023-11-22 21:43:35 +0300

    Draft 2143 22112023

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, Paper, Stack, Typography } from '@mui/material'
+import { FC, useRef, useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const hasGetUserMedia = () => {
+  return !!(navigator?.mediaDevices?.getUserMedia);
+}
+
+export const App: FC = () => {
+  const ref = useRef<HTMLVideoElement | null>(null)
+
+
+  useEffect(() => {
+    if (hasGetUserMedia() && ref.current && ref.current instanceof HTMLVideoElement) {
+
+      navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+        .then(stream => {
+          ref.current!.srcObject = stream
+        })
+        .catch(error => console.error(error))
+
+    }
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container maxWidth='md'>
+      <Stack justifyContent='center' alignItems='center'>
+        <Typography color='text.secondary' variant='h1'>Трансляция</Typography>
+      </Stack>
+      <Paper elevation={12}>
+        <Stack>
+          <video ref={ref} autoPlay />
+        </Stack>
+      </Paper>
+    </Container>
   )
 }
 
-export default App
